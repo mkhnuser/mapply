e61fbba55d03eb2e2c99c5130204193286b17f77
Vladislav Mikheenko <mkhn.email@gmail.com>
2023-09-22 19:43:17 +0300

    Add initial infrastructure.

---
+++ infrastructure/create_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE map_events(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(64) NOT NULL,
+    description VARCHAR(512) NOT NULL,
+    lat REAL NOT NULL CHECK (lat >= -90 AND lat <= 90),
+    lng REAL NOT NULL CHECK (lng >= -180 AND lng <= 180),
+);
